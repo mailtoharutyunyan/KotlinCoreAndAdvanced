6b87bd816e7285a243235a63378f9d55e6d3e018
Arayik Harutyunyan <arayikh@energizeglobal.com>
2019-10-30 16:44:49 +0400

    MyKotlinIsAwesome

---
+++ intro/src/com/hadihariri/kotlincourse/interop/TalkingToKotlin.java
@@ -0,0 +1,41 @@
+package com.hadihariri.kotlincourse.interop;
+
+import com.hadihariri.kotlincourse.classes.CustomerKotlin;
+import com.hadihariri.kotlincourse.classes.Status;
+
+import java.io.IOException;
+
+/**
+ * Created by hadihariri on 25/08/16.
+ */
+public class TalkingToKotlin {
+
+
+    public void loadStats(CustomerKotlin customerKotlin) {
+
+
+  /*      try {
+   //         customerKotlin.loadStatistics("filename");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+    }
+
+    public static void main(String[] args) {
+
+        CustomerKotlin customerKotlin = new CustomerKotlin(1, "Hadi", "mail@hadihariri.com");
+
+        customerKotlin.setEmail("mail@hadihariri.com");
+
+
+        customerKotlin.changeStatus(Status.Current);
+
+        customerKotlin.preferential();
+
+
+        UtilityClass.prefix("some", "value");
+        int copyrightYear = UtilityClass.CopyrightYear;
+
+    }
+}
---
+++ intro/src/com/hadihariri/kotlincourse/stdlib/StringExtensions.kt
@@ -0,0 +1,17 @@
+package com.hadihariri.kotlincourse.stdlib
+
+/**
+ * Created by hadihariri on 25/08/16.
+ */
+
+
+fun main(args: Array<String>) {
+
+
+    "A Message".endsWith()
+
+}
+
+private fun String.endsWith() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
